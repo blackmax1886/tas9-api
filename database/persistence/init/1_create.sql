a221e7eecd5073f5da5e0aa8e142a72da694ef64
blackmax <blackmax1886@gmail.com>
2023-02-08 22:50:44 +0900

    DB: add subtask table

--- database/persistence/init/1_create.sql
+++ database/persistence/init/1_create.sql
@@ -16,5 +16,19 @@ CREATE TABLE IF NOT EXISTS `task` (
 CREATE TABLE IF NOT EXISTS `user` (
   `id` varchar(64) NOT NULL,
   `name` varchar(256) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `subtask` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `parent_task_id` varchar(64) NOT NULL,
+  `content` varchar(256) NOT NULL,
+  `done` boolean NOT NULL,
+  `due` datetime,
+  `assigned_at` datetime,
+  `priority` varchar(16),
+  `archived` boolean NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
