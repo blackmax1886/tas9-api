52b663b584ea7e67db220ef013e48cf361efc6e8
blackmax <blackmax1886@gmail.com>
2023-02-09 17:40:04 +0900

    REFACTOR: define params of dsn

--- db/connection.go
+++ db/connection.go
@@ -1,15 +1,24 @@
 package db
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "localuser:localpass@tcp(127.0.0.1:3306)/localdb?charset=utf8&parseTime=True&loc=Local"
+	USERNAME := "localuser"
+	PASSWORD := "localpass"
+	HOST := "127.0.0.1"
+	PORT := "3306"
+	DATABASE := "localdb"
+
+	dsn := USERNAME + ":" + PASSWORD + "@tcp(" + HOST + ":" + PORT + ")/" + DATABASE + "?charset=utf8mb4" + "&parseTime=True"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err.Error())
+		fmt.Printf("Error connecting to database : error=%v", err)
+		return nil, err
 	}
 
 	return db, nil
